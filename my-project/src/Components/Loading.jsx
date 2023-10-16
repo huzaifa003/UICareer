55780172dce493762feddb4f1d3a0bf237ea19c7
arsalanbashir831 <arsalanbashir831@gmail.com>
2023-10-16 16:39:54 +0500

    editing some stuffs

--- my-project/src/Components/Loading.jsx
+++ my-project/src/Components/Loading.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
 
-const Loading = ({ type, color }) => (
-    <div style={{ alignContent: 'center', alignSelf: 'center', justifyContent: 'center', width: '100%', 'height': '100%' }}>
+const Loading = () => (
+    <div className="flex items-center justify-center h-screen">
         <ReactLoading type={'spin'} color={'red'} height={'20%'} width={'20%'} />
     </div>
 );
